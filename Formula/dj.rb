7c4dff0fdcf1c83ed5610a11f79a5d619fa78f7c
aleontiev <alonetiev@gmail.com>
2017-07-28 16:10:23 -0700

    version 0.0.5

--- Formula/dj.rb
+++ Formula/dj.rb
@@ -1,5 +1,6 @@
 class Dj < Formula
   desc "dj, the Django CLI"
+  version "0.0.5"
   homepage "https://github.com/aleontiev/dj"
   url "https://github.com/aleontiev/dj/releases/download/v0.0.5/dj-0.0.5.tar.gz"
   sha256 "cf3c3ab176383732c205d04b61f9dfb8bcb57f15785490a75ae6a63f37ec5f92"
